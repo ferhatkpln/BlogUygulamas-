daf624461f88f69e9bd2c8c86b3909c0da92fa16
ferhatkpln <kaplanfrht123@gmail.com>
2023-01-05 23:25:58 +0300

    05.01.2023

--- UdemyBlog.Services/Abstract/IArticleService.cs
+++ UdemyBlog.Services/Abstract/IArticleService.cs
@@ -12,9 +12,10 @@ namespace UdemyBlog.Services.Abstract
     public interface IArticleService
     {
         Task<IDataResult<ArticleDto>> Get(int articleId);
-        Task<IDataResult<ArticleListDto>> GetAll();
-        Task<IDataResult<ArticleListDto>> GetAllByNonDeletedAndActiveted();
+        Task<IDataResult<ArticleListDto>> GetAll();       
         Task<IDataResult<ArticleListDto>> GetAllByNonDeleted();
+        Task<IDataResult<ArticleListDto>> GetAllByNonDeletedAndActiveted();
+        Task<IDataResult<ArticleListDto>> GetAllByCategory(int categoryId);
         Task<IResult> Add(ArticleAddDto articleAddDto, string createdByName);
         Task<IResult> Update(ArticleUpdateDto articleUpdateDto, string modifiedByName);
         Task<IResult> Delete(int articleId, string modifiedByName);
